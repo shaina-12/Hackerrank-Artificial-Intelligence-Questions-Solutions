5ecb13051dab34826c3318de58eca6ff3474d67b
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2024-09-16 14:30:50 +0530

    Create Correlation and Regression Lines - A Quick Recap 4.py

---
+++ Correlation and Regression Lines - A Quick Recap 4.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# taken reference from https://www.hackerrank.com/challenges/correlation-and-regression-lines-4/forum
+
+y = 7
+
+# x = ((5*y) - 33)/4
+
+x = ((9*y) + 107)/20
+
+print(round(x, 1))
